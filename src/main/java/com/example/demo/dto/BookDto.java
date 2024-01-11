dfe98d4750b5a20df557c66711300faf7c8b1943
elena <ele-emelina@yandex.ru>
2024-01-12 02:49:53 +0300

    Add Authors

--- src/main/java/com/example/demo/dto/BookDto.java
+++ src/main/java/com/example/demo/dto/BookDto.java
@@ -1,9 +1,12 @@
 package com.example.demo.dto;
 
+import com.example.demo.entity.Author;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,4 +16,5 @@ public class BookDto {
     private Long id;
     private String name;
     private String genre;
+    private List<AuthorDto> authors;
 }
